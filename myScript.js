127fbe7fbb088554ac439101485dee6c9e2e4f4b
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-04-02 14:18:52 -0500

    Added buttons for R,P,S and display items
     -Added Computer Choice and User Choice display in index.html (Lines
     10-11)
     -Added 3 buttons for RPS and Result (lines 13-16)
    
     -Need to rewrite code in myScript.js, so previous code from main
     was removed and I created new variables to store queryselectors
     (lines 4-9)
    
     -Tested userChoiceDisplay and it successfully displays the userChoice
     (lines 12-14)

--- myScript.js
+++ myScript.js
@@ -1,70 +1,16 @@
 
 let playerScore = 0
 let computerScore = 0
+let userChoice
 
- //R,P,S is assigned to numbers 0-2 for cpu
-function computerPlay () {
-    const randomNumber = Math.floor(Math.random() * 3 );
-    switch (randomNumber) {
-        case 0:
-        return 'rock';
-        case 1:
-        return 'paper';
-        case 2:
-        return 'scissors';
-    };
-};
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('game-result')
+const possibleChoices = document.querySelectorAll('button')
 
-//Simulating a round played and adding 1 to the winners score
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return 'The game was a tie!'
-    };
+//When button is clicked, display in user choice
+ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+ }));
 
-    if (playerSelection.toLowerCase() === 'rock') {
-        if (computerSelection === 'paper') {
-            computerScore++;
-            return 'Paper beats rock, the computer won!'
-            
-        } else {
-            playerScore++;
-            return 'Rock beats scissors, you won!'
-        }
-    }
-
-    if (playerSelection.toLowerCase() === 'paper') {
-        if (computerSelection === 'scissors') {
-            computerScore++;
-            return 'Scissors beats paper, the computer won!'
-            
-        } else {
-            playerScore++;
-            return 'Paper beats rock, you won!'
-        }
-    }
-
-    if (playerSelection.toLowerCase() === 'scissors') {
-        if (computerSelection === 'rock') {
-            computerScore++;
-            return 'Rock beats scissors, the computer won!'
-            
-        } else {
-            playerScore++;
-            return 'Scissors beats paper, you won!' 
-        }
-    }
-};
-
-//Set rounds to 5 
-function game () {
-    playRound()
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper, or Scissors?")
-        const computerSelection = computerPlay()
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`Your score: ${playerScore}`)
-        console.log(`Computer score: ${computerScore}`)
-    }
-};
-
-game()
