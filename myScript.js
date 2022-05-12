b2c6f122a0380c7f91fd28848bdeb02588f92976
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-05-11 21:13:25 -0500

    Complete Overhaul of webpage
    -Changed Rock Paper Scissors buttons to emojis.
    -Created a header at the top of the webpage
    -Moved result display above choices

--- myScript.js
+++ myScript.js
@@ -12,19 +12,11 @@ const possibleChoices = document.querySelectorAll('button')
 const userScoreDisplay = document.getElementById('user-score')
 const computerScoreDisplay = document.getElementById('computer-score')
 
-//When button is clicked, display in user choice
- possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    computerPlay();
-    playRound();
-    keepScore();
- }));
 
- //Assign RPS for cpu
- function computerPlay() {
-    const randomNumber = Math.floor(Math.random() * 3 );
-    
+//Assign RPS for cpu
+function computerPlay() {
+    const randomNumber = Math.floor(Math.random() * 3);
+
     if (randomNumber === 0) {
         computerChoice = 'rock'
     }
@@ -35,7 +27,7 @@ const computerScoreDisplay = document.getElementById('computer-score')
         computerChoice = 'scissors'
     }
 
-    computerChoiceDisplay.innerHTML = computerChoice
+    computerChoiceDisplay.innerText = computerChoice
 };
 
 //Game results
@@ -43,40 +35,40 @@ function playRound() {
     if (computerChoice === userChoice) {
         result = ('It\'s a draw!')
     }
-    if (computerChoice === 'rock' &&  userChoice === 'scissors') {
+    if (computerChoice === 'rock' && userChoice === 'scissors') {
         computerScore++
         result = ('rock beats scissors, you lose!')
     }
-    if (computerChoice === 'paper' &&  userChoice === 'rock') {
+    if (computerChoice === 'paper' && userChoice === 'rock') {
         computerScore++
         result = ('paper beats rock, you lose!')
     }
-    if (computerChoice === 'scissors' &&  userChoice === 'paper') {
+    if (computerChoice === 'scissors' && userChoice === 'paper') {
         computerScore++
         result = ('scissors beats paper, you lose!')
     }
-    if (userChoice === 'rock' &&  computerChoice === 'scissors') {
+    if (userChoice === 'rock' && computerChoice === 'scissors') {
         userScore++
         result = ('rock beats scissors, you win!')
     }
-    if (userChoice === 'paper' &&  computerChoice === 'rock') {
+    if (userChoice === 'paper' && computerChoice === 'rock') {
         userScore++
         result = ('paper beats rock, you win!')
     }
-    if (userChoice === 'scissors' &&  computerChoice === 'paper') {
+    if (userChoice === 'scissors' && computerChoice === 'paper') {
         userScore++
         result = ('scissors beats paper, you win!')
     }
 
-    resultDisplay.innerHTML = result
-    userScoreDisplay.innerHTML = userScore
-    computerScoreDisplay.innerHTML = computerScore
-    
+    resultDisplay.innerText = result
+    userScoreDisplay.innerText = userScore
+    computerScoreDisplay.innerText = computerScore
+
 };
 
 //Keep score until 5
 function keepScore() {
-    if (userScore === 5 ) {
+    if (userScore === 5) {
         alert('Game Over. Congrats you won!')
         location.reload()
     }
@@ -85,3 +77,12 @@ function keepScore() {
         location.reload()
     }
 };
+
+//When button is clicked, display in user choice
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerText = userChoice
+    computerPlay();
+    playRound();
+    keepScore();
+}));
